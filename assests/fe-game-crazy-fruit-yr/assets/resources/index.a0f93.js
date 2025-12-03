8d64b80a69e9aaac61d96c4d2a62c48441707dc4
Hamza Bin Khalid <58167535+venomvex@users.noreply.github.com>
2025-12-03 22:27:37 +0500

    Add files via upload

---
+++ assests/fe-game-crazy-fruit-yr/assets/resources/index.a0f93.js
@@ -0,0 +1,1 @@
+System.register("chunks:///_virtual/resources",[],function(){return{execute:function(){}}}),(()=>{var e,r;e="virtual:///prerequisite-imports/resources",r="chunks:///_virtual/resources",System.register(e,[r],function(u,e){return{setters:[function(e){var r,t={};for(r in e)"default"!==r&&"__esModule"!==r&&(t[r]=e[r]);u(t)}],execute:function(){}}})})();
